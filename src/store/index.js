f697e3884942c46908f4a04f68cc5854322bab15
sjodie1157 <sjodie1157@gmail.com>
2024-02-23 12:16:13 +0200

    backend test on product

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+
+let serverURL = 'https://tnfz-backend.onrender.com/';
 
 export default createStore({
   state: {
+    products: null,
   },
   getters: {
+    products(state) {
+      return state.products;
+    }
   },
   mutations: {
+    setProducts(state, value) {
+      state.products = value;
+    }
   },
   actions: {
+    async fetchProducts(context) {
+      let res = await fetch(`${serverURL}items`);
+      let json = await res.json();
+      context.commit('setProducts', json)
+      console.log(context);
+    },
   },
   modules: {
   }
-})
+});
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
-
+        <ul>
+            <li v-for="product in products" :key="product.prodID">
+                {{ product.prodName }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+    computed: {
+        products() {
+            return this.$store.state.products;
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProducts');
+    },
+    methods: {
+
+    },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
